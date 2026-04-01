66a9ca9476b3c8511764554f284627b57f079c3f
Ivan Caldarella <gamein3x@gmail.com>
2026-04-01 15:57:34 +0200

    Terzo esercizio completo

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -5,10 +5,21 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 
 // Dichiara la funzione qui.
-
+function firstLetterChecker(array, letter) {
+    let newArray = [];
+    for (i=0; i<array.length; i++) {
+        let nameCurrent = names[i];
+        let firstLetter = nameCurrent[0];
+        if (firstLetter === letter) {
+            newArray.push(nameCurrent);
+        }
+    }
+    return newArray;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
+console.log(firstLetterChecker(names, "A"));
 
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
