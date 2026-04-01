cf76b72374c919c01d62a53f0720e6ca7bb722c2
Ivan Caldarella <gamein3x@gmail.com>
2026-04-01 16:28:43 +0200

    Esercizio 4 completo

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -4,10 +4,19 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
-
+function vocalsCounter(wordHere) {
+    const vocalString = "aeiouAEIOUàèìòù"
+    let vocals = 0;
+    for (i=0; i<wordHere.length; i++) {
+        if (vocalString.includes(wordHere[i])) { 
+            vocals = vocals + 1;
+        }
+    }
+    return vocals;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+console.log(vocalsCounter(word));
+console.log(`Ci sono ${vocalsCounter(word)} vocali`);
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
