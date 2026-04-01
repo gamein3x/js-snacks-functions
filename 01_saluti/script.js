898648f0e8f8de0c9f45b84823929f08611132f2
Ivan Caldarella <gamein3x@gmail.com>
2026-04-01 15:08:30 +0200

    Primo esercizio

--- 01_saluti/script.js
+++ 01_saluti/script.js
@@ -3,11 +3,14 @@
 const userName = 'Mario';
 
 
-// Dichiara la funzione qui.
 
+// Dichiara la funzione qui.
+function saluti(name) {
+    return `Come va, ${name}?`
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(saluti(userName));
 
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
