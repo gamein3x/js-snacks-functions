355d2d75f25d317d3af63fd713517de4b6bd75aa
Ivan Caldarella <gamein3x@gmail.com>
2026-04-01 16:43:39 +0200

    Esercizio 5 completato, buon pomeriggio

--- 05_bonus/script.js
+++ 05_bonus/script.js
@@ -7,13 +7,28 @@ e buonasera se è sera (oltre le 17)
 */
 
 const name = 'Mario';
-
+let time = -1;
 
 // Dichiara la funzione qui.
-
+function saluter() {
+    let messaggio = "";
+    if (time >= 0 && time < 6) {
+        messaggio = `Dovresti dormire, ${name}`;
+    } else if (time >= 6 && time < 13) {
+        messaggio = `Buongiorno ${name}`;
+    } else if (time >= 13 && time < 17) {
+        messaggio = `Buon pomeriggio ${name}`;
+    } else if (time >= 17 && time < 24) {
+        messaggio = `Buonasera ${name}`;
+    }
+    return messaggio;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+while (time<23) {
+    time++;
+    console.log(`Sono le ${time}. ${saluter()}`);
+}
 
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
